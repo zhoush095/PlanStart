494be55f1208a184a3e733986ec20109b9af09a3
zhoush095 <107750746+zhoush095@users.noreply.github.com>
2023-02-11 13:42:36 +0800

    PlanStart alpha v0.0.1 Programming language: Python Main file: main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import logging
 
 # Logging
 logging.basicConfig(level=logging.INFO,
-                    format='%(levelname)s - %(asctime)s - %(filename)s - %(funcName) - %(message)s')
-# logger = logging.getLogger(__name__)
+                    format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 # Get Time
 time_year_month_day_hour_minute: int = int(time.strftime('%y%m%d%H%M', time.localtime(time.time())))
 time_month_day_hour_minute: int = int(time.strftime('%m%d%H%M', time.localtime(time.time())))
@@ -13,24 +14,27 @@ time_week_hour_minute: int = int(time.strftime('%w%H%M', time.localtime(time.tim
 # variable time_week_hour_minute Week(0-6) is Sunday to Saturday
 time_day_hour_minute: int = int(time.strftime('%d%H%M', time.localtime(time.time())))
 
-logging.info("variable "+"time_year_month_day_hour_minute"+" value is "+str(time_year_month_day_hour_minute))
+# Variable Logging
+logger.info("Variable "+"time_year_month_day_hour_minute"+" value is "+str(time_year_month_day_hour_minute))
+logger.info("Variable "+"time_month_day_hour_minute"+" value is "+str(time_month_day_hour_minute))
+logger.info("Variable "+"time_year_month_day_hour_minute"+" value is "+str(time_year_month_day_hour_minute))
 
-# plan_start_list = []
-# plan_time_list = []
-#
-# i = 0
-# while 1:
-#     input_str = input()
-#     if input_str == 'exit':
-#         break
-#     plan_start_list.append(input_str)
-#     if not os.path.exists(plan_start_list[i]):
-#         print("Path not found")
-#         plan_start_list.remove(input_str)
-#     else:
-#         i += 1
-#
-# for a in range(0, len(plan_start_list)):
-#     os.system('"'+plan_start_list[a]+'"')
-#     # out_str = out.read()
-#     # print(out_str)
+plan_start_list = []
+plan_time_list = []
+
+i = 0
+while 1:
+    input_str = input()
+    if input_str == 'exit':
+        break
+    plan_start_list.append(input_str)
+    if not os.path.exists(plan_start_list[i]):
+        print("Path not found")
+        plan_start_list.remove(input_str)
+    else:
+        i += 1
+
+for a in range(0, len(plan_start_list)):
+    os.system('"'+plan_start_list[a]+'"')
+    # out_str = out.read()
+    # print(out_str)
